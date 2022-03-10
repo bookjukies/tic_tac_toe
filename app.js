7f2e46b5cc395d60d174e1dac9d6d276d0bc2774
Bongani <mkhumbie34@gmail.com>
2022-03-10 14:17:46 +0200

    added the symbole

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const squares = document.querySelectorAll(`.grid-item`)
+function symbol(square){
+    let player = `x`
+    return square.innerHTML = `<p>${player}</p>`
+}
 squares.forEach((square)=>{
-    square.addEventListener(`click`, (event)=>{
-        square.innerHTML=`o`
+    square.addEventListener(`click`, ()=>{
+        symbol(square)
     })
 })
