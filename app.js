42eab56b78a85d711a20c6dbbfd0cfe190dd0f1e
Bongani <mkhumbie34@gmail.com>
2022-03-11 11:18:18 +0200

    added logic to take turns and fill squares

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const squares = document.querySelectorAll(`.grid-item`)
+let turnHandle = 0
+let xcheck = []
+let ocheck = []
+
+function resultCheck(square){
+    console.log(square.id)
+}
+
 function symbol(square){
     let player = `x`
-    return square.innerHTML = `<p>${player}</p>`
+
+    if(square.innerHTML){
+        alert(`Square is taken !!! click empty square`)
+    }else{
+        turnHandle++
+        if (turnHandle%2 == 0){
+            player = `X`
+            xcheck.push(square.id)
+
+            resultCheck(square)
+            square.innerHTML = `<p>${player}</p>` 
+        }else{
+            player = `O`
+            ocheck.push(square.id)
+
+            resultCheck(square)
+            square.innerHTML = `<p>${player}</p>`    
+        }
+        
+    }  
+
 }
+
 squares.forEach((square)=>{
-    square.addEventListener(`click`, ()=>{
+    square.addEventListener(`click`,()=>{
         symbol(square)
     })
 })
